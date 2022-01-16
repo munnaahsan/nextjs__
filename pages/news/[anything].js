306f75b6e98e38359f3eb1bc364eabb7331be2a7
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2022-01-16 10:39:19 +0600

    dynamic page

---
+++ pages/news/[anything].js
@@ -0,0 +1,7 @@
+function Anything() {
+    return <h1>News Anything Page</h1>;
+
+
+}
+
+export default Anything;
---
+++ pages/news/index.js
@@ -0,0 +1,7 @@
+function News() {
+    return <h1>News Index Page</h1>;
+
+
+}
+
+export default News;
